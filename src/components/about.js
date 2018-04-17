7a9dd0d408a6de6d59ca4b690a3ad32b515218b0
Niccolo Ortega <nfcortega89@gmail.com>
2018-04-17 16:06:07 -0700

    updated

--- src/components/about.js
+++ src/components/about.js
@@ -8,15 +8,24 @@ const Wrapper = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: 4em;
 `;
 const AboutH2 = styled.h2`
   -webkit-font-smoothing: antialiased;
-  width: 100%;
   text-transform: uppercase;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 2em 0;
+  margin: 0 0 2em
+  font-size: 1.5em;
+  position: relative;
+`;
+const Brush1 = styled.div`
+  position: absolute;
+  top: -4px;
+  right: -3px;
+  border: 2px solid grey;
+  width: 74px;
 `;
 const AboutBlock = styled.div`
   width: 100%;
@@ -29,13 +38,12 @@ const PictureCol = styled.div`
   display: flex;
   width: 50%;
   height: 100%;
-  padding: 0px 2em;
   justify-content: center;
   align-items: center;
 `;
 const Image = styled.div`
   background: ${props => props.bg};
-  width: 250px;
+  width: 300px;
   height: 500px;
   margin: 0 0.5em;
 `;
@@ -45,13 +53,17 @@ const BioCol = styled.div`
 `;
 const BioP = styled.p`
   width: 100%;
-  padding-right: 8em;
-  line-height: 28px;
+  line-height: 33px;
+  margin: 0;
+  padding: 0 2em;
 `;
 const About = () => {
   return (
     <Wrapper>
-      <AboutH2>About me</AboutH2>
+      <AboutH2>
+        About me
+        <Brush1 />
+      </AboutH2>
       <AboutBlock>
         <PictureCol>
           <Image bg="grey" />
@@ -67,7 +79,16 @@ const About = () => {
             remaining essentially unchanged. It was popularised in the 1960s
             with the release of Letraset sheets containing Lorem Ipsum passages,
             and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's standard dummy text ever since the 1500s, when
+            an unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently
+            with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.
           </BioP>
         </BioCol>
       </AboutBlock>
