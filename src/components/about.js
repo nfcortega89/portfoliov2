9a3174acdda12540d999a6be12d43937cc14f5c4
Niccolo Ortega <nfcortega89@gmail.com>
2018-04-19 12:01:25 -0700

    added bio

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import desk from '../assets/desk.jpg';
+import aboutpic from '../assets/aboutpic.jpg';
 
 const Wrapper = styled.section`
   width: 100%;
@@ -49,9 +51,12 @@ const PictureCol = styled.div`
   align-items: center;
 `;
 const Image = styled.div`
-  background: ${props => props.bg};
+  background-image: url(${props => props.src});
+  background-size: ${props => props.size};
+  background-position: ${props => props.position}
+  background-repeat: no-repeat;
   width: 300px;
-  height: 500px;
+  height: 430px;
   margin: 0 0.5em;
 `;
 const BioCol = styled.div`
@@ -61,7 +66,7 @@ const BioCol = styled.div`
 const BioP = styled.p`
   width: 100%;
   line-height: 33px;
-  margin: 0;
+  margin: 1em 0;
   padding: 0 2em;
 `;
 const About = () => {
@@ -74,29 +79,29 @@ const About = () => {
       </AboutH2>
       <AboutBlock>
         <PictureCol>
-          <Image bg="grey" />
-          <Image bg="yellow" />
+          <Image src={desk} size="290% 110%;" />
+          <Image src={aboutpic} size="138% 110%;" position="60% 70%" />
         </PictureCol>
         <BioCol>
           <BioP>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply
-            dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s, when
-            an unknown printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries, but
-            also the leap into electronic typesetting, remaining essentially
-            unchanged. It was popularised in the 1960s with the release of
-            Letraset sheets containing Lorem Ipsum passages, and more recently
-            with desktop publishing software like Aldus PageMaker including
-            versions of Lorem Ipsum.
+            Hi my name is Niccolo Ortega -a frontend developer based in Los
+            Angeles and recent grad from Thinkfuls' intensive mentorship
+            program. It was there I was able to learn web developing create
+            fullstack applications with the awesome power of Node.js and usage
+            of React for the frontend.
+          </BioP>
+          <BioP>
+            I primarily focus on writing clean, elegant, and efficient code and
+            I am proficient in HTML, CSS, Javascript, and Wordpress. Although I
+            am in the earlier stages of my career, my background in web
+            developing along with my education in Entrepreneurship have
+            re-enforced my logical skills as well as being more resilient and
+            paying close attention to detail.
+          </BioP>
+          <BioP>
+            I am also highly motivated and having the have the eagerness to
+            learn. When I am not coding on projects, I am usually doing a coding
+            challenge and picking up classes in Udemy to hone my skills further.
           </BioP>
         </BioCol>
       </AboutBlock>
---
+++ src/components/testimonial.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const TestimonialBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  background: #282828;
+  padding: 2em 2em 2em 7em;
+  position: relative;
+`;
+const TestimonialRight = styled.div`
+  width: 50%;
+`;
+const TestimonialH2 = styled.h2`
+  width: 100%;
+  color: white;
+  text-transform: uppercase;
+  -webkit-font-smoothing: antialiased;
+  font-size: 1.5em;
+  position: relative;
+`;
+const Brush = styled.hr`
+  width: ${props => props.width}
+  position: absolute;
+  bottom: ${props => props.bottom};
+  left: ${props => props.left};
+  right: ${props => props.right};
+  top: ${props => props.top};
+  border: 2px solid #404040;
+`;
+const TestimonialP = styled.p`
+  line-height: 28px;
+  color: white;
+  -webkit-font-smoothing: antialiased;
+  width: 100%;
+  margin: 2em 0;
+`;
+const Name = styled.p`
+  color: white;
+  width: 100%;
+  -webkit-font-smoothing: antialiased;
+  font-weight: 800;
+  text-transform: uppercase;
+  font-size: 17px;
+`;
+const Title = styled.p`
+  color: white;
+  width: 100%;
+  -webkit-font-smoothing: antialiased;
+  margin: 0;
+`;
+const Decoration = styled.p`
+  position: absolute;
+  top: -89px;
+  right: 21px;
+  margin: 0;
+  font-size: 290px;
+  color: #404040;
+  letter-spacing: -30px;
+  font-family: 'Secular One', sans-serif;
+`;
+const Testimonial = () => {
+  return (
+    <Wrapper>
+      <TestimonialBlock>
+        <TestimonialH2>
+          Testimonials
+          <Brush width="83px" bottom="-17px" left="1px" />
+          <Brush width="80px" top="-16px" right="383px" />
+        </TestimonialH2>
+        <TestimonialP>
+          Nikko is an ambitious, driven and excellent problem solver. As his
+          mentor, he has been an absolute pleasure to work with as he constantly
+          strives to achieve his goals creatively while using best practices.
+          His ability to work through difficult problems is impressive and never
+          gives up. I highly recommend Nikko as a full-stack developer!
+        </TestimonialP>
+        <Name>Arthur Longbottom</Name>
+        <Title>
+          Sr. React / React Native Developer at Omni Holding Company
+        </Title>
+        <Decoration>''</Decoration>
+      </TestimonialBlock>
+      <TestimonialRight />
+    </Wrapper>
+  );
+};
+
+export default Testimonial;
